354eb8d9c285a5f35c31017c2ea034d547b9cc7d
ismail-tellioglu <ismail.tellioglu@techsin.com.tr>
2019-10-04 10:09:57 +0300

    Add project files.

---
+++ myfirstAzureWebApp/Context/CoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myfirstAzureWebApp.Context
+{
+    public class CoreDbContext:DbContext
+    {
+        public CoreDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ myfirstAzureWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using myfirstAzureWebApp.Context;
+
+namespace myfirstAzureWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private CoreDbContext con;
+        public List<Employee> employess { get; set; }
+        public IndexModel (CoreDbContext _con)
+        {
+            con = _con;
+        }
+        public void OnGet()
+        {
+             employess = con.Employees.ToList();
+        }
+    }
+}
